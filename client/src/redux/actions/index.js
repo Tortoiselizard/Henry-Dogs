c52b8aa8dabf0e4ad634f3d47469ba9c5d2aa32e
Troy Pernía <troy00pernia@gmail.com>
2022-12-11 14:20:13 -0400

    Se creó la carpeta "redux" con las subcarpetas: "actions", "reducer" y "store", y sus respectivos archivos js. En el archivo asociado a las "actions" se agregaron los action-generators: "getAllDogs", "getDogDetail" y "createDog"

---
+++ client/src/redux/actions/index.js
@@ -0,0 +1,34 @@
+export const GET_ALL_DOGS = "GET_ALL_DOGS"
+export const GET_DOG_DETAILS = "GET_DOG_DETAILS"
+export const CREATE_DOG = "CREATE_DOG"
+export const GET_TEMPERAMENTS = "GET_TEMPERAMENTS"
+
+// 🟢 getAllDogs:
+// Esta función debe realizar una petición al Back-End. Luego despachar una action con la data recibida. End-Point: 'http://localhost:3001/dogs'.
+export const getAllDogs = () => {
+    return function (dispatch) {
+        return fetch('http://localhost:3001/dogs')
+        .then((response) => response.json())
+        .then((data) => {dispatch({ type: GET_ALL_DOGS, payload: data})});
+        }
+}
+
+// 🟢 getDogDetail:
+// Esta función debe hacer una petición al Back-End. Ten en cuenta que tiene que recibir la variable "id" por parámetro. Luego despachar una action con la data recibida. End-Point: 'http://localhost:3001/dogs/:idRaza'.
+export const getDogDetail = (id) => {
+    return function (dispatch) {
+        return fetch(`http://localhost:3001/dogs/${id}`)
+        .then((response) => response.json())
+        .then((data) => dispatch({ type: GET_DOG_DETAILS, payload: data}));
+        }
+};
+
+// 🟢 createDog: Esta función debe recibir una variable "dog" por parámetro. Luego retornar una action que, en su propiedad payload:
+//    - haga un spread operator de la variable dog, para copiar todo su contenido.
+
+export const createDog = (dog) => {
+    return {
+        type: CREATE_DOG,
+        payload: {...dog}
+    }
+};
