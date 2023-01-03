5d36db085b77c4bc57f9e95d992cff4902987590
Troy Pernía <troy00pernia@gmail.com>
2023-01-03 15:55:31 -0400

    Se creo el archivo DogDetail.module.css y se importo al componente DogDetail.jsx. Ademas se modifico en el componente, en la funcion "React.useEffect()", para que modificara el global state, vaciando la propiedad dogDetail. Por otra parte se restructuro el renderizado del componente para que fuera acorde a los estilos importados.

--- client/src/components/DogDetail/DogDetail.jsx
+++ client/src/components/DogDetail/DogDetail.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import {useSelector, useDispatch} from 'react-redux'
 import {getDogDetail, cleanDetail} from "../../redux/actions/index"
+import style from "./DogDetail.module.css"
 
 function DogDetail() {
 
@@ -12,25 +13,32 @@ function DogDetail() {
     // console.log(raza_perro)
     const dispatch = useDispatch()
 
-    React.useEffect(async ()=> {
+    React.useEffect(()=> {
         // console.log(id)
-        const action = getDogDetail(raza_perro)
-        if (typeof(action.payload) === "string") {return alert(action.payload)}
-        dispatch(action)
+        // console.log(raza_perro)
+        // const action = await getDogDetail(raza_perro)
+        // console.log(action.payload)
+        // if (typeof(action.payload) === "string") {return alert(action.payload)}
+        // else {dispatch(action)}
+        dispatch(getDogDetail(raza_perro))
         return function() {
             dispatch(cleanDetail())
         }
     },[raza_perro])
 
     return <div>
-        <h1>Estoy en el detalle del perro DogDetail</h1>
-        {Object.keys(dogDetail).length?<div>
-            <h1>{dogDetail.name}</h1>
-            <img src={dogDetail.image} alt={dogDetail.name}></img>
-            <p>Temperaments: <span>{dogDetail.temperament}</span></p>
-            <p>Height (cm): <span>{dogDetail.height}</span></p>
-            <p>Weight (kg): <span>{dogDetail.weight}</span></p>
-            <p>years: <span>{dogDetail.life_span}</span></p>        
+        {/* <h1>Estoy en el detalle del perro DogDetail</h1> */}
+        {Object.keys(dogDetail).length?<div className={style.DogDetail}>
+            <label>
+                <img className={style.imagen} src={dogDetail.image} alt={dogDetail.name}></img>
+            </label>
+            <div>
+                <h1 className={style.name}>{dogDetail.name}</h1>
+                <label className={style.temperamentos}><span>Temperaments: </span><p>{dogDetail.temperament}</p></label>
+                <label className={style.alto}><span>Height: </span><p>{dogDetail.height}</p></label>
+                <label className={style.peso}><span>Weight (kg): </span><p>{dogDetail.weight}</p></label>
+                <label className={style.years}><span>years: </span><p>{dogDetail.life_span}</p></label>        
+            </div>
         </div>:null}
     </div>
 };
